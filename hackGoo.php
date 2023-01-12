3cf7f8b71c5050d3547eff1e36b2a76e86e66b50
kwynncom <buessresume@gmail.com>
2023-01-11 23:39:10 -0500

    goo cracked

--- hackGoo.php
+++ hackGoo.php
@@ -1,9 +1,16 @@
 <?php
 
 require_once('/opt/kwynn/kwutils.php');
+require_once('/opt/kwynn/crackObject.php');
+
 
 $j = file_get_contents('/var/kwynn/gooauth/t');
 $a = json_decode($j, true);
+
+$a = crackObject::crackGoo($a);
+$f = kwifs($a, "Google_Clienttoken");
+
+kwynn();
 
 // 		if (substr($p, 0, 3) === "\x00*\x00") return substr($p, 3);
 
