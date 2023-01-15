edd13ee791af6ef58781ff2aec41bf0d27e62fce
kwynncom <buessresume@gmail.com>
2023-01-15 02:17:11 -0500

    rework of usage class

--- usageLimit/usageLimitDao.php
+++ usageLimit/usageLimitDao.php
@@ -2,49 +2,48 @@
 
 
 require_once(__DIR__ . '/../configDB.php');
-require_once(__DIR__ . '/../dao.php');
 
-class daoUsage implements qemconfig {
+class daoUsage extends dao_generic_3 implements qemconfig {
     
     private $iid = null;
     
     function __construct() {
-		$this->client = new MongoDB\Client();
-		$this->usage = $this->client->selectCollection(dao_plain::dbname, 'usage');
-		$this->usage->deleteMany(['time' => ['$lt' => time() - 86400 * 30]]);
+		parent::__construct(qemconfig::dbname);
+		$this->creTabs('usage');
+		$this->ucoll->deleteMany(['U' => ['$lt' => time() - 86400 * 30]]);
     }
     
     private function setIID($res) { $this->iid = $res->getInsertedId();  }
     
     public function setEmail($email) {
 		if (!isset($this->iid)) return;
-		$res = $this->usage->updateOne(['_id' => $this->iid], ['$set' => ['email' => $email]]);
+		$res = $this->ucoll->updateOne(['_id' => $this->iid], ['$set' => ['email' => $email]]);
     }
 
 	private static function setSessionDets(&$o) {
-	    $o->ip    = isset($_SERVER['REMOTE_ADDR'    ]) ? $_SERVER['REMOTE_ADDR'    ] : null ;
-	    $o->agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
-		$o->sid   = vsid();
+	    $o['ip']    = isset($_SERVER['REMOTE_ADDR'    ]) ? $_SERVER['REMOTE_ADDR'    ] : null ;
+	    $o['agent'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
+		$o['sid']   = vsid();
 	}
 
     public function putUse($type) {
 
-		$ddbo  = new stdClass();
-		$utime  = microtime(true);
-		$dateo = new DateTime();
-		$ddbo->timed = new stdClass();
-		$ddbo->timed->dateo = $dateo;
-		$ddbo->timed->time  = intval($dateo->format('U'));
-		$ddbo->timed->utime = doubleval($dateo->format('U.u'));
-		$ddbo->timed->uonly = doubleval('0.' . $dateo->format('u'));
-		$ddbo->on = $dateo->format('D, M j, g:i:s A (Y +0.u') . 's)';
-		self::setSessionDets($ddbo);
+		$dat = [];
+		$U = time();
+		$dat['r'] = date('r', $U);
+		$dat['U']  = $U;
+		
+		self::setSessionDets($dat);
 
-		$ddbo->type = $type;
+		$dat['type'] = $type;
 
-		$res = $this->usage->insertOne($ddbo);
+		$res = $this->ucoll->insertOne($dat, ['kwnoup' => true]);
 		$this->setIID($res);
     }
         
-    public function getUsage($span, $type) { return $this->usage->count(['timed.utime' => ['$gte' => microtime(true) - $span], 'type' => $type]);   }
+    public function getUsage($span, $type) { 
+		$res = $this->ucoll->count(['U' => ['$gte' => time() - $span], 'type' => $type]);   
+		return $res;
+		
+	}
 }
