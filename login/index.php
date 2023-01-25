6835d2f55d498323dc292b3f96e34499287e3253
kwynncom <buessresume@gmail.com>
2023-01-24 22:53:52 -0500

    switching between email and login seems to work

--- login/index.php
+++ login/index.php
@@ -3,7 +3,7 @@
 require_once(__DIR__ . '/../positiveEmailDefaults.php');
 require_once(__DIR__ . '/../OAuthGooOuter.php');
 
-$goooa = new GooOAUTHWrapperOuter(positiveEmailDefaults::peoaa);
+$goooa = new GooOAUTHWrapperOuter(positiveEmailDefaults::peoaa, __FILE__);
 $url = $goooa->getOAuthURL();
 kwynn();
 
@@ -11,3 +11,4 @@ if ($url) { header('Location: ' . $url); exit(0); }
 
 header('Content-Type: text/plain');
 echo('Logged in as ' . $goooa->emailAddressFromGoo);
+exit(0);
--- receiveAuthCodeActual.php
+++ receiveAuthCodeActual.php
@@ -1,15 +1,4 @@
 <?php
 
-// the switch is just for testing
-if (FALSE && (time() > strtotime('2023-01-24 02:20'))) {
-	require_once('positiveEmail.php');
-	new positiveEmailCl();
-	kwynn();
-}
-else if (FALSE) {
-	require_once(__DIR__ . '/login/index.php');
-}
-
 require_once('OAuthGooState.php');
-
 GooOAuthState::doit();
